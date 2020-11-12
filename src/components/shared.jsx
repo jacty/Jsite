40b5855ff27afecf9e82470b501ce098d77bf627
jacty <jacty0219@qq.com>
2020-11-12 19:01:33 +0800

    Implement Jeact into shared.jsx

--- src/components/shared.jsx
+++ src/components/shared.jsx
@@ -1,5 +1,4 @@
 //Shared Components like Footer, Header and so on.
-// import React from 'react';
 import Jeact from '@Jeact';
 
 const React = Jeact;
@@ -8,6 +7,3 @@ export function Footer(props){
     const year = new Date().getFullYear()
     return <footer>© {year} Jacty</footer>
 }
-
-console.error('Footer', Footer());
-// Jeact.createElement('footer', null, '\xA9', year, 'Jacty');

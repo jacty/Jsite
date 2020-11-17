58bd1242983190cdecb2a5f4b5e4f120354e1e1e
jacty <jacty0219@qq.com>
2020-11-18 02:10:02 +0800

    Bugfix: Change ErrorBoundary to function component

--- src/components/Errors/ErrorBoundary.jsx
+++ src/components/Errors/ErrorBoundary.jsx
@@ -1,22 +1,10 @@
-import React from 'react';
+import {
+    createElement,
+} from '@Jeact';
+const React = {createElement:createElement};
 
-class ErrorBoundary extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {hasError: false};
-    }
-    static getDerivedStateFromError(error){
-        return {hasError: true};
-    }
-    componentDidCatch(error, errorInfo){
-        console.error(error,errorInfo);
-    }
-    render(){
-        if(this.state.hasError){
-            return <h1>Ooops! Something is wrong.</h1>
-        }
-        return this.props.children;
-    }
+function ErrorBoundary(props){
+    return props.children;
 }
 
 export default ErrorBoundary;

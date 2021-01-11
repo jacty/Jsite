082fe825059458da5e9e9e1161226eabed216f7c
jacty <jacty0219@qq.com>
2021-01-11 22:56:23 +0800

    Update path of pages

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -3,7 +3,7 @@ import {J, createRoot} from '@Jeact';
 import ErrorBoundary from '@com/Errors/ErrorBoundary.jsx';
 import '@assets/styles/main.sass';
 
-import Aboutme from '@com/aboutme.jsx';
+import Aboutme from './aboutme.jsx';
 import {Footer} from '@com/shared.jsx';
 
 createRoot(document.getElementById('root')).render(

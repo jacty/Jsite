5d4a7260c11abe88c1f6f1a688a13101c0179e66
jacty <jacty0219@qq.com>
2021-01-11 23:36:41 +0800

    Add style for mottoe

--- src/assets/js/aboutme.jsx
+++ src/assets/js/aboutme.jsx
@@ -1,11 +1,11 @@
 import { J } from '@Jeact';
 import Avatar from '@com/Avatar/avatar.jsx';
-
+import '@assets/styles/aboutme.sass';
 function Aboutme(){
     return <section className='aboutme'>
                 <Avatar />
                 <h1>Jacty</h1>
-                <h2 className='motto'>No one is coming!</h2>
+                <quote className='motto'>No one is coming!</quote>
             </section>
 }
 

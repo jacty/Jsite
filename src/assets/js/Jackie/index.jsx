923e24bcc3ac657bfe5ecda3709aec3c315b62a2
jacty <jacty0219@qq.com>
2021-01-14 16:20:59 +0800

    beta version of film list

--- src/assets/js/Jackie/index.jsx
+++ src/assets/js/Jackie/index.jsx
@@ -3,15 +3,19 @@ import data from '@data/Jackie.json';
 
 export default function Jackie(prop){
     const people = data.people;
+    const keys = data.keys;
     const movies = data.movies;
     const lan = prop.lan === 'en' ? 0 : 1;
-    console.error('x', data);
+
     const list = movies.map((item)=>{
         const path = `./assets/imgs/${item.name[0]}`;
         return <li key={item.id}>
                     <img src={path + '/poster.webp'}  alt={item.name[lan]} title={item.name[lan]} width='135'/>
                     <div>
                         {item.name[lan]}
+                    </div>
+                    <div>
+                        {keys[0][lan]+': '+ people[item.director][lan]}
                     </div>
                 </li>
     })

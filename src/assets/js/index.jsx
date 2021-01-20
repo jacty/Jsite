d692140591bac753b01282d9c1108061cc1df905
jacty <jacty0219@qq.com>
2021-01-20 10:34:11 +0800

    Add intro to about me and style

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -3,12 +3,12 @@ import {J, createRoot} from '@Jeact';
 import ErrorBoundary from '@com/Errors/ErrorBoundary.jsx';
 import '@assets/styles/main.sass';
 
-import Jackie from './Jackie/index.jsx';
+import Aboutme from './aboutme.jsx';
 import {Footer} from '@com/shared.jsx';
 
 createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
-          <Jackie lan={'en'}></Jackie>
+          <Aboutme></Aboutme>
           <Footer />
      </ErrorBoundary>
 )

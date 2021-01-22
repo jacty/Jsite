97a943bfa10cb5146ae97c497ddebddb9fead70b
jacty <jacty0219@qq.com>
2021-01-22 16:34:07 +0800

    1

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 
 import ErrorBoundary from '@com/Errors/ErrorBoundary.jsx';
 import '@assets/styles/main.sass';
 
-import Aboutme from '@com/aboutme.jsx';
+// import Aboutme from '@com/aboutme.jsx';
 import {Footer} from '@com/shared.jsx';
+
+const Aboutme = React.lazy(()=>import('@com/aboutme.jsx'));
 
 ReactDOM.createRoot = ReactDOM.unstable_createRoot;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <ErrorBoundary>
-         <Aboutme></Aboutme>
-         <Footer />
-    </ErrorBoundary>
+    <Suspense fallback={<div>demo</div>}>
+        <Aboutme></Aboutme> 
+    </Suspense>
 )
 

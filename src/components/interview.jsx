4dbc65a8570b7724a6494c29fb5a3658e0f0d5ec
jacty <jacty0219@qq.com>
2021-01-23 15:44:22 +0800

    1

--- src/components/interview.jsx
+++ src/components/interview.jsx
@@ -1,69 +1,69 @@
-import {
-    createElement,
-    useState,
-} from '@Jeact';
-const React = {createElement:createElement};
+// import {
+//     createElement,
+//     useState,
+// } from '@Jeact';
+// const React = {createElement:createElement};
 
-import '@assets/styles/interview.sass';
-// const List = React.lazy(()=> import('@com/List/list.jsx'));
+// import '@assets/styles/interview.sass';
+// // const List = React.lazy(()=> import('@com/List/list.jsx'));
 
-import {rnd} from '@assets/js/helpers/nums';
+// import {rnd} from '@assets/js/helpers/nums';
     
-let flag = 0; //1: show all questions; 0: show a random question
-const mask = 1; // Toggle mask;
+// let flag = 0; //1: show all questions; 0: show a random question
+// const mask = 1; // Toggle mask;
 
-function Interview(props){
-    document.title='Interview Q&A';
-    const items = props.data;
+// function Interview(props){
+//     document.title='Interview Q&A';
+//     const items = props.data;
 
-    const [ind, setInd] = useState(0);
+//     const [ind, setInd] = useState(0);
 
-    const btnAllTxt = ['All Questions','Random Question'];
-    const btnTitleTxt = ['List all questions','Show a random question'];
+//     const btnAllTxt = ['All Questions','Random Question'];
+//     const btnTitleTxt = ['List all questions','Show a random question'];
 
-    function handleClickNext(){
-        let _ind = rnd(0, items.length-1);
+//     function handleClickNext(){
+//         let _ind = rnd(0, items.length-1);
 
-        //Avoid repeating the same question;
-        while(_ind===ind){
-            _ind = rnd(0, items.length-1);
-        }
-        setInd(_ind);
-    }
+//         //Avoid repeating the same question;
+//         while(_ind===ind){
+//             _ind = rnd(0, items.length-1);
+//         }
+//         setInd(_ind);
+//     }
 
-    function handleClickAll(){
-        flag = flag ^ mask;
-        flag > 0?setInd(-1):handleClickNext();
-    }
+//     function handleClickAll(){
+//         flag = flag ^ mask;
+//         flag > 0?setInd(-1):handleClickNext();
+//     }
 
-    let questions;
-    if(flag===1){//List all questions.
-        questions = <List items={props.data} />
-    } else{// Choose a random question.
-        questions = <div>
-            <h2>{items[ind].header}</h2>
-            <div dangerouslySetInnerHTML={{__html:items[ind].content}}></div>
-        </div>
-    }
+//     let questions;
+//     if(flag===1){//List all questions.
+//         questions = <List items={props.data} />
+//     } else{// Choose a random question.
+//         questions = <div>
+//             <h2>{items[ind].header}</h2>
+//             <div dangerouslySetInnerHTML={{__html:items[ind].content}}></div>
+//         </div>
+//     }
 
-    return (
-            <div>
-               <h1>Interview Q&A</h1>
-               {/*<Suspense fallback={<div className='loading'>Loading...</div>}>
-                    {flag === 0 ?
-                        <button title='Choose a random question' 
-                            onClick={()=>handleClickNext()}>Next
-                        </button>:
-                        ""
-                    }
-                    <button title={btnTitleTxt[flag]}
-                        onClick={()=>handleClickAll()}>{btnAllTxt[flag]}
-                    </button>*/}
-                    {questions}
-                {/*</Suspense>*/}
-            </div>
-        )
-}
+//     return (
+//             <div>
+//                <h1>Interview Q&A</h1>
+//                {/*<Suspense fallback={<div className='loading'>Loading...</div>}>
+//                     {flag === 0 ?
+//                         <button title='Choose a random question' 
+//                             onClick={()=>handleClickNext()}>Next
+//                         </button>:
+//                         ""
+//                     }
+//                     <button title={btnTitleTxt[flag]}
+//                         onClick={()=>handleClickAll()}>{btnAllTxt[flag]}
+//                     </button>*/}
+//                     {questions}
+//                 {/*</Suspense>*/}
+//             </div>
+//         )
+// }
 
-export default Interview;
+// export default Interview;
 

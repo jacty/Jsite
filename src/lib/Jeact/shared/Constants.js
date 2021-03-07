7d72b9de74c24a69d248da0f2c64ee77d4b1818d
jacty <jacty0219@qq.com>
2021-03-08 05:32:25 +0800

    Update flags to 24 bits

--- src/lib/Jeact/shared/Constants.js
+++ src/lib/Jeact/shared/Constants.js
@@ -48,23 +48,23 @@ export const InputDiscreteLanePriority = 2;
 /* JeactUpdateQueue.js */
 export const UpdateState = 0;
 /* JeactFiberFlags.js */
-export const NoFlags = /*                      */ 0b00000000000000000000;
-export const PerformedWork = /*                */ 0b00000000000000000001;
-export const Placement = /*                    */ 0b00000000000000000010;
-export const Update = /*                       */ 0b00000000000000000100;
+export const NoFlags = /*                      */ 0b0000000000000000000000;
+export const PerformedWork = /*                */ 0b0000000000000000000001;
+export const Placement = /*                    */ 0b0000000000000000000010;
+export const Update = /*                       */ 0b0000000000000000000100;
 export const PlacementAndUpdate = /*           */ Placement | Update;
-export const Deletion = /*                     */ 0b00000000000000001000;
-export const ChildDeletion = /*                */ 0b00000000000000010000;
-export const ContentReset = /*                 */ 0b00000000000000100000; 
-export const Ref = /*                          */ 0b00000000000100000000;
-export const Snapshot = /*                     */ 0b00000000001000000000;
-export const Passive = /*                      */ 0b00000000010000000000;
+export const Deletion = /*                     */ 0b0000000000000000001000;
+export const ChildDeletion = /*                */ 0b0000000000000000010000;
+export const ContentReset = /*                 */ 0b0000000000000000100000; 
+export const DidCapture = /*                   */ 0b0000000000000010000000;
+export const Ref = /*                          */ 0b0000000000000100000000;
+export const Snapshot = /*                     */ 0b0000000000001000000000;
+export const Passive = /*                      */ 0b0000000000010000000000;
 
-export const HostEffectMask = /*               */ 0b00000001111111111111;
 
 // These are not really side effects, but we still reuse this field.
-export const Incomplete = /*                   */ 0b00000010000000000000;
-export const ShouldCapture = /*                */ 0b00000100000000000000;
+export const Incomplete = /*                   */ 0b0000000010000000000000;
+export const ShouldCapture = /*                */ 0b0000000100000000000000;
 export const BeforeMutationMask = Update | Snapshot | ChildDeletion;
 export const MutationMask = 
     Placement | 

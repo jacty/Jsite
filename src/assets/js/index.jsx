277a7edcb743d9bed169bcb15ea5be2a8b71ff87
jacty <jacty0219@qq.com>
2021-03-31 01:08:39 +0800

    1

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -1,21 +1,19 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 
-import {App} from '@assets/js/app.jsx';
 import '@assets/styles/main.sass';
-
-// import Aboutme from '@com/aboutme.jsx';
-
-
 const Aboutme = React.lazy(()=>import('@com/aboutme.jsx'));
 
-ReactDOM.createRoot = ReactDOM.unstable_createRoot;
+function App(){
+    return (
+        <div>
+            <Suspense fallback={<div>fallback</div>}>
+                <Aboutme />
+            </Suspense>
+        </div>
+    )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <div>
-        <Suspense fallback={<div>fallback</div>}>
-            <Aboutme />
-        </Suspense>
-    </div>
-)
+ReactDOM.createRoot = ReactDOM.unstable_createRoot;
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 

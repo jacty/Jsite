6e4ac73a7ff684b42f8f59cb8758a114e3683545
jacty <jacty0219@qq.com>
2021-04-08 10:56:23 +0800

    1

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -1,13 +1,22 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import ReactDOM from 'react-dom';
 
 import '@assets/styles/main.sass';
 const Aboutme = React.lazy(()=>import('@com/aboutme.jsx'));
 
+function App(){
+    const [cur, setCur] = useState(0);
+    return(
+        <section>
+            <p>{cur}</p>
+            <a onClick={()=>{setCur(2)}}></a>
+        </section>
+        )
+}
+
+
 ReactDOM.createRoot = ReactDOM.unstable_createRoot;
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Suspense fallback={<div>fallback</div>}>
-                <Aboutme />
-    </Suspense>
+    <App />
 )
 

853e570ccc1b68e3661302b7ca31fd1d6aa3c917
jacty <jacty0219@qq.com>
2021-04-29 00:37:04 +0800

    Big cleanup to initial Project JC
        Code removed are too generally and not project based.

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -4,31 +4,14 @@ import {
     Suspense,
     lazy, 
 } from '@Jeact';
-
-const Avatar = lazy(() => import('@com/Avatar/avatar'));
+import Aboutme from '@com/Aboutme/aboutme';
+import {Footer} from '@com/shared';
 import '@assets/styles/main.sass';
 
 function App(){
     return (<div className='page'>
-                <header>
-                    <Suspense fallback={<div>Loading</div>}>
-                    <Avatar />
-                    </Suspense> 
-                    <h1>Jacty</h1>
-                    <p>React developer keen on adapting React philosophy to practice. Make it practical and simple.</p>  
-                </header>
-                <main>
-                    <section className='projects'>
-                        <h2>What I am working on:</h2>
-                        <ul>
-                            <li>
-                                <h3>Jsite</h3>
-                                <p>This is a project I am currently working hard on and what you are scrolling. The intention is 1) I want to build my own website in which I will type every piece of code by myself rather than copy&paste or just import others' libs. 2) Front end has been changed massively, the flaw of which is we have to import loads of unused code just to finish a feature regardless how easy the feature is. So I want to make my code as easy, simple and clear as I can and this supports the 1st intention too. </p>
-                                <p>It is interesting and chanllenge. This project currently is supported by [<b>Jeact.js</b>] which is my own mini version of [<b>React.js</b>].</p>
-                            </li>
-                        </ul>
-                    </section>
-                </main>
+                <Aboutme />
+                <Footer />
             </div>
            )
     

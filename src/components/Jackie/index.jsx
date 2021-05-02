c9a289c68a25a97b0440b4e245558d7ebf3d5a7d
jacty <jacty0219@qq.com>
2021-05-02 15:39:06 +0800

    first post initialize

--- src/components/Jackie/index.jsx
+++ src/components/Jackie/index.jsx
@@ -1,29 +1,12 @@
 import { J } from '@Jeact';
-// import data from '@data/Jackie.json';
+import Post from './Posts/20120509';
 
-// import '@assets/styles/jackie.sass';
+import './main.sass';
 
 export default function Jackie(prop){
-    // const people = data.people;
-    // const keys = data.keys;
-    // const movies = data.movies;
-    // const lan = prop.lan === 'en' ? 0 : 1;
-    return <h1>Jackie</h1>
-    // const list = movies.map((item)=>{
-    //     const path = `./assets/imgs/${item.name[0]}`;
-    //     return <li key={item.id}>
-    //                {/* <img className='poster' src={path + '/poster.webp'}  alt={item.name[lan]} title={item.name[lan]} width='135'/>
-    //                 <div className='info'>
-    //                     <h2>{item.name[lan]}</h2>
-    //                     <div>
-    //                         <em>{keys[0][lan]+': '}</em> 
-    //                         <span>{people[item.directors][lan]}</span>
-    //                     </div>
-    //                 </div>*/}
-    //             </li>
-    // })
-    // return <div>
-    //             <h1>Jackie's films</h1>
-    //             <ul className='jackie'>{list}</ul>
-    //         </div>
+    return (
+        <main className='jackie'>
+            <Post />
+        </main>
+    )
 }
